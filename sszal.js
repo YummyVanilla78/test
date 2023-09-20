0613041a3416f3cec96763e04482431cabdff875
바닐라 <126580801+YummyVanilla78@users.noreply.github.com>
2023-09-21 00:34:42 +0900

    Update sszal.js

--- sszal.js
+++ sszal.js
@@ -1,20 +1,30 @@
-if (window.location.pathname.slice(1)) {
+ if (window.location.pathname.slice(1)) {
       window.location.href = 'http://404.sszal.kro.kr';
     }
-    document.addEventListener("DOMContentLoaded", function () {
-      const sszalImage = document.getElementById("sszal");
-      const gifCursor = document.getElementById("gifCursor");
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.body;
+    const sszalImage = document.getElementById("sszal");
+    const gifCursor = document.getElementById("gifCursor");
 
-      sszalImage.addEventListener("mouseenter", () => {
-        gifCursor.style.display = "block";
-      });
+    // Change background image to gif
+    body.style.backgroundImage = 'url("https://ifh.cc/g/rolG7K.gif")';
 
-      sszalImage.addEventListener("mouseleave", () => {
-        gifCursor.style.display = "none";
-      });
-
-      document.addEventListener("mousemove", (e) => {
-        gifCursor.style.left = e.clientX + "px";
-        gifCursor.style.top = e.clientY + "px";
-      });
+    sszalImage.addEventListener("mouseenter", () => {
+      gifCursor.style.display = "block";
     });
+
+    sszalImage.addEventListener("mouseleave", () => {
+      gifCursor.style.display = "none";
+    });
+
+    document.addEventListener("mousemove", (e) => {
+      gifCursor.style.left = e.clientX + "px";
+      gifCursor.style.top = e.clientY + "px";
+    });
+
+    // Change background image to #333 after 2 seconds
+    setTimeout(() => {
+      body.style.backgroundImage = 'none';
+      body.style.backgroundColor = '#333';
+    }, 2000);
+  });
